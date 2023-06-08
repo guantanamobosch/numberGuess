c642e3ce542a0d3fd9d0a9e51795fcd7960f34b3
guantanamobosch <luketyson17@gmail.com>
2023-06-07 18:09:17 -0600

    Sudocode

---
+++ README.md
@@ -0,0 +1,3 @@
+# C++ Number Guesser
+
+This is a program written in C++ which allows the user to play a number guessing game with the computer.
---
+++ numberGuess.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Initialize variables representing the lower and upper limits of the computer's range of numbers to be randomly selected
+    // Initialize a variable representing the random number that the computer generates
+    // Initialize a variable representing the user's guess
+
+    // Take input from the user for the lower and upper limits
+    // Computer chooses a random number
+
+    // Prompt user for their first guess and check if it is correct (if so, output congratulations)
+    // Second guess...
+    // Third guess...
+
+    // If none of the guesses were correct, then output sorry
+
+    return 0;
+}
